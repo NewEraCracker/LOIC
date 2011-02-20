faed5f5b7d6ca5afe9e04a51b9db06ae227c19f9
NewEraCracker <neweracracker@gmail.com>
2011-02-20 19:06:57 +0000

    Version 1.1.1.24 - IRC connection and TCP/UDP method improvements

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: System.CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.1.1.23")]
+[assembly: AssemblyVersion("1.1.1.24")]
