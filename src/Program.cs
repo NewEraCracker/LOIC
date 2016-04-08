d18aa594637665ee0006a3eefcb7ee447bc18ea3
NewEraCracker <neweracracker@gmail.com>
2016-04-08 20:55:08 +0100

    More randomness

--- src/Program.cs
+++ src/Program.cs
@@ -17,7 +17,7 @@ namespace LOIC
 			string ircchannel = "";
 			/* Lets try this! */
 			int count = 0;
-			foreach (string s in cmdLine)
+			foreach(string s in cmdLine)
 			{
 				/* IRC */
 				if(s.ToLower() == "/hivemind")
@@ -25,29 +25,31 @@ namespace LOIC
 					hive = true;
 					ircserver = cmdLine[count + 1]; //if no server entered let it crash
 					try {ircport = cmdLine[count + 2];}
-					catch (Exception) {ircport = "6667";} //default
+					catch(Exception) {ircport = "6667";} //default
 					try {ircchannel = cmdLine[count + 3];}
-					catch (Exception) {ircchannel = "#loic";} //default
+					catch(Exception) {ircchannel = "#loic";} //default
 				}
 				/* Lets try this! */
-				if (s.ToLower() == "/hidden") {hide = true;}
+				if(s.ToLower() == "/hidden") {hide = true;}
 				count++;
 			}
-			//Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new frmMain(hive, hide, ircserver, ircport, ircchannel));
 		}
 	}
 	public class Functions
 	{
+		private static Random rnd = new Random();
+		private static String[] ntv = {"6.0","6.1","6.2","6.3","10.0"};
+
 		public static string RandomString()
 		{
 			StringBuilder builder = new StringBuilder();
-			Random random = new Random();
+
 			char ch;
-			for (int i = 0; i < 6; i++)
+			for(int i = 0; i < 6; i++)
 			{
-				ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+				ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * rnd.NextDouble() + 65)));
 				builder.Append(ch);
 			}
 			return builder.ToString();
@@ -55,16 +57,14 @@ namespace LOIC
 
 		public static string RandomUserAgent()
 		{
-			Random random = new Random();
-
-			String[] osversions  = {"5.1","6.0","6.1"};
-			String[] oslanguages = {"en-GB","en-US","es-ES","pt-BR","pt-PT","sv-SE"};
-
-			String version   = osversions[ random.Next(0, osversions.Length-1) ];
-			String language  = oslanguages[ random.Next(0, oslanguages.Length-1) ];
-			String useragent = String.Format("Mozilla/5.0 (Windows; U; Windows NT {0}; {1}; rv:1.9.2.17) Gecko/20110420 Firefox/3.6.17", version, language);
-
-			return useragent;
+			if(rand.NextDouble() >= 0.5)
+			{
+				return String.Format("Mozilla/5.0 (Windows NT {0}; WOW64; rv:{1}.0) Gecko/20100101 Firefox/{1}.0", ntv[rnd.Next(0, ntv.Length-1)], rnd.Next(36, 46));
+			}
+			else
+			{
+				return String.Format("Mozilla/5.0 (Windows NT {0}; rv:{1}.0) Gecko/20100101 Firefox/{1}.0", ntv[rnd.Next(0, ntv.Length-1)], rnd.Next(36, 46));
+			}
 		}
 	}
 }
