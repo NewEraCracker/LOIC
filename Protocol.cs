5a093dd1b0bf209be80e6d92c5a15bc2e3058b0d
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2017-09-20 09:53:38 +0100

    Add files via upload
    Adding handler to enum for Slow Loris

---
+++ Protocol.cs
@@ -0,0 +1,41 @@
+/* LOIC - Low Orbit Ion Cannon
+ * Released to the public domain
+ * Enjoy getting v&, kids.
+ */
+
+using System;
+
+namespace LOIC {
+
+	/// <summary>
+	/// Protocol.
+	/// </summary>
+	public enum Protocol
+	{
+		/// <summary>
+		/// No (invalid) protocol.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// Transmission Control Protocol.
+		/// </summary>
+		TCP = 1,
+
+		/// <summary>
+		/// User Datagram Protocol.
+		/// </summary>
+		UDP = 2,
+
+		/// <summary>
+		/// HyperText Transfer Protocol.
+		/// </summary>
+		HTTP = 3,
+
+
+        ///<summary>
+        ///Slow loris connections attack
+        ///</summary>
+        SlowLoris = 4,
+	}
+}
